76ff4492c309d054c69f0394c85fef5985c5c2ed
Takuro Breath <maxsmurffy@gmail.com>
2023-08-05 20:18:26 +0300

    updated channel.py

--- src/channel.py
+++ src/channel.py
@@ -3,6 +3,7 @@ import os
 from googleapiclient.discovery import build
 import isodate
 
+
 class Channel:
     """Класс для ютуб-канала"""
 
@@ -10,12 +11,71 @@ class Channel:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
         self.channel_id = channel_id
 
+    @property
+    def channel_id(self):
+        return self.__channel_id
+
+    @channel_id.setter
+    def channel_id(self, value):
+        self.__channel_id = value
+
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        api_key : str = os.getenv("API_KEY")
+        api_key: str = os.getenv("API_KEY")
         youtube = build('youtube', 'v3', developerKey=api_key)
-        channel_id = 'UCwHL6WHUarjGfUM_586me8w'  # HighLoad Channel
-        channel = youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
+        channel = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
         print(json.dumps(channel, indent=2, ensure_ascii=False))
 
+    @property
+    def title(self):
+        api_key: str = os.getenv("API_KEY")
+        youtube = build('youtube', 'v3', developerKey=api_key)
+        channel = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        return channel['items'][0]['snippet']['title']
 
+    @property
+    def video_count(self):
+        api_key: str = os.getenv("API_KEY")
+        youtube = build('youtube', 'v3', developerKey=api_key)
+        channel = youtube.channels().list(id=self.channel_id, part='statistics').execute()
+        return channel['items'][0]['statistics']['videoCount']
+
+    @property
+    def url(self):
+        return f"https://www.youtube.com/channel/{self.channel_id}"
+
+
+
+    @property
+    def description(self):
+        api_key: str = os.getenv("API_KEY")
+        youtube = build('youtube', 'v3', developerKey=api_key)
+        channel = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        return channel['items'][0]['snippet']['description']
+
+    @property
+    def subscribers(self):
+        api_key: str = os.getenv("API_KEY")
+        youtube = build('youtube', 'v3', developerKey=api_key)
+        channel = youtube.channels().list(id=self.channel_id, part='statistics').execute()
+        return channel['items'][0]['statistics']['subscriberCount']
+
+    @property
+    def views(self):
+        api_key: str = os.getenv("API_KEY")
+        youtube = build('youtube', 'v3', developerKey=api_key)
+        channel = youtube.channels().list(id=self.channel_id, part='statistics').execute()
+        return channel['items'][0]['statistics']['viewCount']
+
+    @staticmethod
+    def get_service():
+        api_key: str = os.getenv("API_KEY")
+        youtube = build('youtube', 'v3', developerKey=api_key)
+        return youtube
+
+    def to_json(self, filename):
+        api_key: str = os.getenv("API_KEY")
+        youtube = build('youtube', 'v3', developerKey=api_key)
+        channel = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        with open(filename, "w") as f:
+            json.dump(channel, f)
